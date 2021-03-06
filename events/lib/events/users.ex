3579e7604f41db894b385558e66aa00b44c6645e
mchadha716 <41342617+mchadha716@users.noreply.github.com>
2021-03-05 22:18:15 -0500

    Update users.ex

--- events/lib/events/users.ex
+++ events/lib/events/users.ex
@@ -39,8 +39,8 @@ defmodule Events.Users do
 
   def get_user(id), do: Repo.get(User, id)
 
-  def get_user_by_email(email) do
-    Repo.get_by(User, email: email)
+  def get_user_by_name(name) do
+    Repo.get_by(User, name: name)
   end
 
   @doc """
