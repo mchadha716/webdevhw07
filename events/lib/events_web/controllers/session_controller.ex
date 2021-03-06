2deed84087c506081b4eaad2b2bccd61694c26da
mchadha716 <41342617+mchadha716@users.noreply.github.com>
2021-03-05 21:52:22 -0500

    Update session_controller.ex

--- events/lib/events_web/controllers/session_controller.ex
+++ events/lib/events_web/controllers/session_controller.ex
@@ -1,8 +1,8 @@
 defmodule EventsWeb.SessionController do
   use EventsWeb, :controller
 
-  def create(conn, %{"email" => email}) do
-    user = Events.Users.get_user_by_email(email)
+  def create(conn, %{"name" => name}) do
+    user = Events.Users.get_user_by_name(name)
     if user do
       conn
       |> put_session(:user_id, user.id)
@@ -22,4 +22,3 @@ defmodule EventsWeb.SessionController do
     |> redirect(to: Routes.page_path(conn, :index))
   end
 end
-
