078d9e3321326e18f93408466388009a5642390d
mchadha716 <41342617+mchadha716@users.noreply.github.com>
2021-03-05 22:22:46 -0500

    Update users.ex

--- events/lib/events/users.ex
+++ events/lib/events/users.ex
@@ -10,12 +10,9 @@ defmodule Events.Users do
 
   @doc """
   Returns the list of users.
-  
   ## Examples
-  
       iex> list_users()
       [%User{}, ...]
-      
   """
   def list_users do
     Repo.all(User)
@@ -23,17 +20,12 @@ defmodule Events.Users do
 
   @doc """
   Gets a single user.
-  
   Raises `Ecto.NoResultsError` if the User does not exist.
-  
   ## Examples
-  
       iex> get_user!(123)
       %User{}
-      
       iex> get_user!(456)
       ** (Ecto.NoResultsError)
-      
   """
   def get_user!(id), do: Repo.get!(User, id)
 
@@ -45,15 +37,11 @@ defmodule Events.Users do
 
   @doc """
   Creates a user.
-  
   ## Examples
-  
       iex> create_user(%{field: value})
       {:ok, %User{}}
-      
       iex> create_user(%{field: bad_value})
       {:error, %Ecto.Changeset{}}
-      
   """
   def create_user(attrs \\ %{}) do
     %User{}
@@ -63,15 +51,11 @@ defmodule Events.Users do
 
   @doc """
   Updates a user.
-  
   ## Examples
-  
       iex> update_user(user, %{field: new_value})
       {:ok, %User{}}
-      
       iex> update_user(user, %{field: bad_value})
       {:error, %Ecto.Changeset{}}
-      
   """
   def update_user(%User{} = user, attrs) do
     user
@@ -81,15 +65,11 @@ defmodule Events.Users do
 
   @doc """
   Deletes a user.
-  
   ## Examples
-  
       iex> delete_user(user)
       {:ok, %User{}}
-      
       iex> delete_user(user)
       {:error, %Ecto.Changeset{}}
-      
   """
   def delete_user(%User{} = user) do
     Repo.delete(user)
@@ -97,12 +77,9 @@ defmodule Events.Users do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking user changes.
-  
   ## Examples
-  
       iex> change_user(user)
       %Ecto.Changeset{data: %User{}}
-      
   """
   def change_user(%User{} = user, attrs \\ %{}) do
     User.changeset(user, attrs)
